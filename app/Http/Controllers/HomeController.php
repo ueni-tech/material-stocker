ff855aef5bdfb48023a5f1ccf086192ef113eab7
uenishi <ueni.tech@gmail.com>
2024-11-17 13:10:07 +0900

    #7 一覧画面にキーワード表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
     {
         $files = Image::with('majorCategory')->get()->map(function ($image) {
             $image->major_category = $image->majorCategory->name;
+            $image->minor_categories = $image->minorCategories()->pluck('name')->toArray();
             return $image;
         })->sortByDesc('created_at');
         return view('index', compact('files'));
