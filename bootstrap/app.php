d60b4fdc8ad93319dab47bba44265f974d38ae7c
uenishi <ueni.tech@gmail.com>
2024-12-21 20:58:31 +0900

    Revert "#9 api用の設定"
    This reverts commit c914aea790e6c6b235e51887c5a4da601fb981da.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -16,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth' => CheckAuth::class,
         ]);
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
