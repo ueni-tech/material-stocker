30e00cc8d67e9ac790e5e81b0408561d42ac8bc8
uenishi <ueni.tech@gmail.com>
2025-01-14 17:38:53 +0900

    #13 ページネーション仮置き

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,29 +8,36 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function home()
-    {
-        $files = Image::with('majorCategory')->get()->map(function ($image) {
-            $image->major_category = $image->majorCategory->name;
-            $image->minor_categories = $image->minorCategories()->pluck('name')->toArray();
-            $image->formatted_date = Carbon::parse($image->created_at)->format('Y年m月d日');
-            $image->is_mine = $image->user_id === auth()->user()->id;
-            return $image;
-        })->sortByDesc('created_at');
+  public function home()
+  {
+    $files = Image::with('majorCategory')
+      ->orderBy('created_at', 'desc')
+      ->paginate(12)
+      ->through(function ($image) {
+        $image->major_category = $image->majorCategory->name;
+        $image->minor_categories = $image->minorCategories()->pluck('name')->toArray();
+        $image->formatted_date = Carbon::parse($image->created_at)->format('Y年m月d日');
+        $image->is_mine = $image->user_id === auth()->user()->id;
+        return $image;
+      });
 
-        return view('pages.home', compact('files'));
-    }
+    return view('pages.home', compact('files'));
+  }
 
-    public function mine()
-    {
-        $files = Image::where('user_id', auth()->user()->id)->with('majorCategory')->get()->map(function ($image) {
-            $image->major_category = $image->majorCategory->name;
-            $image->minor_categories = $image->minorCategories()->pluck('name')->toArray();
-            $image->formatted_date = Carbon::parse($image->created_at)->format('Y年m月d日');
-            $image->is_mine = $image->user_id === auth()->user()->id;
-            return $image;
-        })->sortByDesc('created_at');
+  public function mine()
+  {
+    $files = Image::where('user_id', auth()->user()->id)
+      ->with('majorCategory')
+      ->orderBy('created_at', 'desc')
+      ->paginate(12)
+      ->through(function ($image) {
+        $image->major_category = $image->majorCategory->name;
+        $image->minor_categories = $image->minorCategories()->pluck('name')->toArray();
+        $image->formatted_date = Carbon::parse($image->created_at)->format('Y年m月d日');
+        $image->is_mine = $image->user_id === auth()->user()->id;
+        return $image;
+      });
 
-        return view('pages.mine', compact('files'));
-    }
+    return view('pages.mine', compact('files'));
+  }
 }
